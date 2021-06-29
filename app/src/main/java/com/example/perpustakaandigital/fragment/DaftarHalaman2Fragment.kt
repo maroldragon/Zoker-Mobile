4fccd8237e1a86a19e465f1389a6b53cd5f2dd72
Evan Owen Pasaribu <evanowenpasaribu7@gmail.com>
2021-06-29 16:08:50 +0700

    Detail

---
+++ app/src/main/java/com/example/perpustakaandigital/fragment/DaftarHalaman2Fragment.kt
@@ -0,0 +1,19 @@
+package com.example.perpustakaandigital.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.perpustakaandigital.R
+
+class DaftarHalaman2Fragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_daftar_halaman2, container, false)
+    }
+
+}
